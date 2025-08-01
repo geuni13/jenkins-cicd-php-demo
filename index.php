e34a420bd4bd465775b2189a498e524e1578a631
geuni13 <geuni13@nate.com>
2025-08-02 03:33:11 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     echo "사이트 도메인 : ".$_SERVER['SERVER_NAME'];
     echo "<br><br>";
     echo "클라이언트 IP : ".$_SERVER['REMOTE_ADDR'];
-    echo "<br><br>";
+    echo "<br><hr><br>";
     $arrayWeek = array('일','월','화','수','목','금','토');
     echo "현재시간 : ".date('Y-m-d')." (".$arrayWeek[date('w')].") ".date('H:i:s');
 ?>
