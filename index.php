44f1efd0d833108a7299a51c941b8c13c5f7105c
YG <geuni13@gmail.com>
2025-08-02 13:13:32 +0900

    test freestyle ssh

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     echo "클라이언트 IP : ".$_SERVER['REMOTE_ADDR'];
     echo "<br><br>";
     echo "<hr>";
-    echo "<br><br>";
     $arrayWeek = array('일','월','화','수','목','금','토');
     echo "현재시간 : ".date('Y-m-d')." (".$arrayWeek[date('w')].") ".date('H:i:s');
 ?>
