d38269bdd0924828b76de2dc6e891660f09df2a4
YG <geuni13@gmail.com>
2025-08-03 05:36:05 +0900

    수정

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
     echo "<h2>". ENVIRONMENT ."</h2>";
 
-    echo "<br><br>";
+    echo "<br><hr><br>";
     echo "사이트 도메인 : ".$_SERVER['SERVER_NAME'];
     echo "<br><br>";
     echo "클라이언트 IP : ".$_SERVER['REMOTE_ADDR'];
